0717956cdcce6db6eb651a8367949bbb51f47ff5
Vijaychandra-7781 <92368094+Vijaychandra-7781@users.noreply.github.com>
2021-10-13 09:13:05 +0400

    Create employee.py
    Add employee details

---
+++ employee.py
@@ -0,0 +1,17 @@
+class employee:
+  def __init__(self,id,name,email,salary,address,phone):
+    id=self.id
+    name=self.name
+    email=self.email
+    salary=self.salary
+    address=self.address
+    phone=self.phone
+  def display(self):
+    print("id:",self.id)
+    print("name:",self.name)
+    print('email:',self.email)
+    print('salary:',self.salry)
+    print('address:',slef.address)
+    print('phone:',self.pone)
+e1=employee(id=1,name,'vijay',email='vijay@gmail.com',salary=101010,address='bangalore',phone=4567898765)
+e1.display()
