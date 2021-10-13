e6ba279533f1ea3c7ed3a3efa71eb52ce6c40597
Vijaychandra M c <vijaychandra7781@gmail.com>
2021-10-13 10:54:35 +0400

    Adding one attribute in this commit

--- employee.py
+++ employee.py
@@ -1,17 +1,19 @@
 class employee:
-  def __init__(self,id,name,email,salary,address,phone):
+  def __init__(self,id,name,email,salary,address,phone,bond):
     id=self.id
     name=self.name
     email=self.email
     salary=self.salary
     address=self.address
     phone=self.phone
+    bond=self.bond
   def display(self):
     print("id:",self.id)
     print("name:",self.name)
     print('email:',self.email)
     print('salary:',self.salry)
     print('address:',slef.address)
-    print('phone:',self.pone)
+    print('phone:',self.phone)
+    print('bond:',self.bond)
 e1=employee(id=1,name,'vijay',email='vijay@gmail.com',salary=101010,address='bangalore',phone=4567898765)
 e1.display()
